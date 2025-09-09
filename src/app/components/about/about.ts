7d82d3e5b9ade6e6ed8526ecfdaa561ff19235fd
Divya Shukla <divyashukla515@gmail.com>
2025-09-09 22:11:28 +0530

    changes

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './about.scss'
 })
 export class About {
+
+  constructor(private router: Router) {}
+
+  navigateToContact() {
+    this.router.navigate(['/contact']);
+  }
 
 }
