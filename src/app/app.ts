2ec64a51f55f1b2bc188b6dae9c3f53eb8282acc
Divya Shukla <divyashukla515@gmail.com>
2025-09-11 22:32:28 +0530

    changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,51 @@
+// app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet
-  ],
   templateUrl: './app.html',
-  styleUrls: ['./app.scss']
+  imports:[FormsModule]
 })
-export class AppComponent {}
+export class AppComponent {
+  formData = { name: '', email: '', message: '' };
+
+  // sendEmail(e: Event) {
+  //   e.preventDefault();
+  //   emailjs
+  //     .send(
+  //       'service_vo424jh',
+  //       'template_cm4pp9b',
+  //       this.formData,
+  //       'apbmLEz0Ir3KF0xi5'
+  //     )
+  //     .then(
+  //       (result: EmailJSResponseStatus) => {
+  //         console.log('SUCCESS!', result.status, result.text);
+  //         alert('Message Sent!');
+  //       },
+  //       (error) => {
+  //         console.log('FAILED...', error);
+  //         alert('Error sending message');
+  //       }
+  //     );
+  // }
+  submitForm(formData: any) {
+  fetch("https://script.google.com/macros/s/AKfycbz2thVnQd6blJLJvYdp8bkn5TWWaiCiQhj6oVr6Hb4BIBJFHeF2gwtdOjYCMr_JcYMW/exec", {
+    method: "POST",
+    mode: "no-cors", // ✅ bypass CORS
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData)
+  });
+
+  alert("Submitted! (Saved in Google Sheet, but no response shown)");
+}
+}
+
+
+
+
+
+
+
