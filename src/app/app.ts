2c2f54e6fe054a33e87f424e08c6913820f8358e
Divya Shukla <divyashukla515@gmail.com>
2025-09-11 12:44:02 +0530

    merged

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
-import { Hero } from './components/hero/hero';
-import { About } from './components/about/about';
-import { Brands } from './components/brands/brands';
-import { Clients } from './components/clients/clients';
-import { Footer } from './components/footer/footer';
-import { Services } from './components/services/services';
-import { Testimonials } from './components/testimonials/testimonials';
-import { FadeInDirective } from './directives/fade-in';
-import { ParallaxDirective } from './directives/parallax';
-import { Portfolio } from './portfolio/portfolio';
 
 @Component({
   selector: 'app-root',
