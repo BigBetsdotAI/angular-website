5c319a870171353267f9803c8601ad47fd35b1a0
Divya Shukla <divyashukla515@gmail.com>
2025-09-11 19:15:21 +0530

    testing email.js

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
-import { Hero } from './components/hero/hero';
-import { About } from './components/about/about';
-import { Brands } from './components/brands/brands';
-import { Clients } from './components/clients/clients';
-import { Footer } from './components/footer/footer';
-import { Services } from './components/services/services';
-import { Testimonials } from './components/testimonials/testimonials';
-import { FadeInDirective } from './directives/fade-in';
-import { ParallaxDirective } from './directives/parallax';
-import { Portfolio } from './portfolio/portfolio';
 
 @Component({
   selector: 'app-root',
@@ -22,3 +11,53 @@ import { Portfolio } from './portfolio/portfolio';
   styleUrls: ['./app.scss']
 })
 export class AppComponent {}
+
+// app.component.ts
+// import { Component } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.html',
+//   imports:[FormsModule]
+// })
+// export class AppComponent {
+//   formData = { name: '', email: '', message: '' };
+
+//   sendEmail(e: Event) {
+//     e.preventDefault();
+//     emailjs
+//       .send(
+//         'service_1dgiuxb',
+//         'template_cm4pp9b',
+//         this.formData,
+//         'apbmLEz0Ir3KF0xi5'
+//       )
+//       .then(
+//         (result: EmailJSResponseStatus) => {
+//           console.log('SUCCESS!', result.status, result.text);
+//           alert('Message Sent!');
+//         },
+//         (error) => {
+//           console.log('FAILED...', error);
+//           alert('Error sending message');
+//         }
+//       );
+//   }
+//   submitForm() {
+//     fetch('https://script.google.com/macros/s/AKfycbz2thVnQd6blJLJvYdp8bkn5TWWaiCiQhj6oVr6Hb4BIBJFHeF2gwtdOjYCMr_JcYMW/exec', {
+//       method: 'POST',
+//       body: JSON.stringify(this.formData),
+//       headers: { 'Content-Type': 'application/json' }
+//     })
+//     .then(res => res.text())
+//     .then(res => alert('Saved successfully in Google Sheet!'))
+//     .catch(err => console.error(err));
+//   }
+// }
+
+
+
+
+
