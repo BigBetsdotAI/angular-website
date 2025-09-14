ca745e73eaf64d3bddf5223f40029002426656bb
Divya Shukla <divyashukla515@gmail.com>
2025-09-14 21:47:07 +0530

    responsive-css

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,33 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
-import { Hero } from './components/hero/hero';
-import { About } from './components/about/about';
-import { Brands } from './components/brands/brands';
-import { Clients } from './components/clients/clients';
-import { Footer } from './components/footer/footer';
-import { Services } from './components/services/services';
-import { Testimonials } from './components/testimonials/testimonials';
-import { FadeInDirective } from './directives/fade-in';
-import { ParallaxDirective } from './directives/parallax';
-import { Portfolio } from './portfolio/portfolio';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    Header,
-    Hero,
-    Brands,
-    About,
-    Services,
-    Testimonials,
-    Clients,
-    Footer,
-    ParallaxDirective,
-    FadeInDirective,
-    Portfolio
+    RouterOutlet
   ],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
